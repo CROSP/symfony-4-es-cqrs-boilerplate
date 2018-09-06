dd7b506c246945c08d505dd8bffc59d6282dc72a
Alexander <alexander@crosp.net>
2018-09-06 14:43:41 +0300

    Initial commit

--- src/UI/Http/Web/Controller/AbstractRenderController.php
+++ src/UI/Http/Web/Controller/AbstractRenderController.php
@@ -1,55 +1,50 @@
 <?php
 
-declare(strict_types=1);
+declare( strict_types=1 );
 
 namespace App\UI\Http\Web\Controller;
 
 use League\Tactician\CommandBus;
 use Symfony\Component\HttpFoundation\Response;
 
-class AbstractRenderController
-{
-    /**
-     * @throws \Twig_Error_Loader
-     * @throws \Twig_Error_Runtime
-     * @throws \Twig_Error_Syntax
-     */
-    protected function render(string $view, array $parameters = [], int $code = Response::HTTP_OK): Response
-    {
-        $content = $this->template->render($view, $parameters);
+class AbstractRenderController {
+	/**
+	 * @throws \Twig_Error_Loader
+	 * @throws \Twig_Error_Runtime
+	 * @throws \Twig_Error_Syntax
+	 */
+	protected function render( string $view, array $parameters = [], int $code = Response::HTTP_OK ): Response {
+		$content = $this->template->render( $view, $parameters );
 
-        return new Response($content, $code);
-    }
+		return new Response( $content, $code );
+	}
 
-    protected function exec($command): void
-    {
-        $this->commandBus->handle($command);
-    }
+	protected function exec( $command ): void {
+		$this->commandBus->handle( $command );
+	}
 
-    protected function ask($query)
-    {
-        return $this->queryBus->handle($query);
-    }
+	protected function ask( $query ) {
+		return $this->queryBus->handle( $query );
+	}
 
-    public function __construct(\Twig_Environment $template, CommandBus $commandBus, CommandBus $queryBus)
-    {
-        $this->template = $template;
-        $this->commandBus = $commandBus;
-        $this->queryBus = $queryBus;
-    }
+	public function __construct( \Twig_Environment $template, CommandBus $commandBus, CommandBus $queryBus ) {
+		$this->template   = $template;
+		$this->commandBus = $commandBus;
+		$this->queryBus   = $queryBus;
+	}
 
-    /**
-     * @var CommandBus
-     */
-    private $commandBus;
+	/**
+	 * @var CommandBus
+	 */
+	private $commandBus;
 
-    /**
-     * @var CommandBus
-     */
-    private $queryBus;
+	/**
+	 * @var CommandBus
+	 */
+	private $queryBus;
 
-    /**
-     * @var \Twig_Environment
-     */
-    private $template;
+	/**
+	 * @var \Twig_Environment
+	 */
+	private $template;
 }
